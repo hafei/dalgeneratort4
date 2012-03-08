59451f7478a15cd5c30d26c1ac3486899d34df3b
syed.bashar <syed.bashar@localhost>
2012-03-08 18:24:16 +0600

    Added to version control

---
+++ DataGenerationSolution/DataGenerationSolution/DataGeneratorTest1/CountryGeneratorsTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using DG.Common;
+using DataGenerator;
+using NUnit.Framework;
+
+namespace DataGeneratorTest1
+{
+    [TestFixture]
+    class CountryGeneratorsTest
+    {
+        private CountryRepository _countryRepository;
+        [SetUp]
+        public void SetUp()
+        {
+            _countryRepository = new CountryRepository();
+        }
+        [Test]
+        public void GetAllOperations()
+        {
+            var countries = _countryRepository.GetAll();
+            Assert.IsFalse(countries.Count == 0);
+        }
+
+        [Test]
+        public void GetById()
+        {
+            var country = _countryRepository.GetById(4);
+            Assert.IsFalse(country.Id == 0);
+        }
+        [Test]
+        public void Insert()
+        {
+            var country = new Country
+                              {
+                                  HasState = true,
+                                  ISO = "BC",
+                                  Name = "ABC",
+                                  PrintableName = "ABC",
+                                  ISO3 = "ABC"
+                              };
+            long insert = _countryRepository.Insert(country);
+            Assert.IsFalse(insert == 0);
+        }
+        [Test]
+        public void Update()
+        {
+            var country = new Country
+                              {
+                Id = 245,
+                HasState = true,
+                ISO = "CC",
+                Name = "ABC",
+                PrintableName = "ABC",
+                ISO3 = "ABC"
+            };
+            _countryRepository.Update(country);
+            Assert.IsFalse(false);
+        }
+        [Test]
+        public void AllbySort()
+        {
+            var shortItems = new List<SortItem> {new SortItem {ColumnName = "ISO", SortOrder = SortOrder.DESC}};
+            var sortDefinitions= new SortDefinitions();
+            sortDefinitions.SoftItems.AddRange(shortItems);
+            List<Country> allBySorting = _countryRepository.GetAllBySorting(sortDefinitions);
+            Assert.IsFalse(allBySorting.Count==0);
+        }
+        [Test]
+        public void SortWithPagination()
+        {
+            var shortItems = new List<SortItem> { new SortItem { ColumnName = "ISO", SortOrder = SortOrder.DESC } };
+            var sortDefinitions = new SortDefinitions();
+            sortDefinitions.SoftItems.AddRange(shortItems);
+            var allBySorting = _countryRepository.GetAllBySortingAndPaged(sortDefinitions,5,20);
+            Assert.IsFalse(allBySorting.Count != 20);
+        }
+    }
+}
---
+++ DataGenerationSolution/DataGenerationSolution/DataGeneratorTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataGeneratorTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DataGeneratorTest1")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ede55d17-adb4-4acf-96bb-af0b77647172")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
